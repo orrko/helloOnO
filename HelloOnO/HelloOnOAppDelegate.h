315b0755b65dfb9edb07a3816910000f249cc9d4
orr kowarsky <OnOApps@new-host-2.home>
2011-08-07 17:25:06 +0300

    Initial Commit

---
+++ HelloOnO/HelloOnOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloOnOAppDelegate.h
+//  HelloOnO
+//
+//  Created by orr kowarsky on 8/7/11.
+//  Copyright 2011 Orr Kowarsky Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloOnOAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ HelloOnO/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloOnO
+//
+//  Created by orr kowarsky on 8/7/11.
+//  Copyright 2011 Orr Kowarsky Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
